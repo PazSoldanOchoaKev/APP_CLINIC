8cd7c7ebfdce543a94b43e7af4964e9296ccb512
t03861 <stalinpazsoldanochoa@bcp.com.pe>
2022-05-12 13:38:10 -0500

    changes

--- AppointmentManager/AppointmentManager/Resources/Controls/PageBase.xaml.cs
+++ AppointmentManager/AppointmentManager/Resources/Controls/PageBase.xaml.cs
@@ -1,15 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
-
+using System.ComponentModel;
 using Xamarin.Forms;
 
 namespace AppointmentManager.Resources.Controls
 {
+    [DesignTimeVisible(true)]
+    [ContentProperty("ContentBody")]
     public partial class PageBase : ContentPage
     {
         public PageBase()
         {
             InitializeComponent();
+        }
+
+        public static readonly BindableProperty HeaderTitleProperty =
+            BindableProperty.Create(nameof(HeaderTitle), typeof(string), typeof(PageBase), default(string));
+
+        public string HeaderTitle
+        {
+            get => (string)GetValue(HeaderTitleProperty);
+            set => SetValue(HeaderTitleProperty, value);
+        }
+
+        public static readonly BindableProperty ContentBodyProperty =
+             BindableProperty.Create(nameof(ContentBody), typeof(View), typeof(PageBase));
+
+        public View ContentBody
+        {
+            get => (View)GetValue(ContentBodyProperty);
+            set => SetValue(ContentBodyProperty, value);
         }
     }
 }
--- AppointmentManager/AppointmentManager/Views/Access/SingInView.xaml.cs
+++ AppointmentManager/AppointmentManager/Views/Access/SingInView.xaml.cs
@@ -1,11 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
-
+using AppointmentManager.Resources.Controls;
 using Xamarin.Forms;
 
 namespace AppointmentManager.Views.Access
 {
-    public partial class SingInView : ContentPage
+    public partial class SingInView : PageBase
     {
         public SingInView()
         {
