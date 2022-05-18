e8a3f19c8e2e8077e8ba72ffde5beda20c8646ac
Kevin Paz Soldan Ochoa <kevin_pazsoldan@hotmail.com>
2022-05-18 02:49:36 -0500

    RegistroMascota

---
+++ AppointmentManager/AppMobile/AppointmentManager/Views/Access/AnimalInformationView.xaml.cs
@@ -0,0 +1,21 @@
+﻿using AppointmentManager.Resources.Controls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppointmentManager.Views.Access
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class AnimalInformationView : PageBase
+    {
+        public AnimalInformationView()
+        {
+            InitializeComponent();
+        }
+    }
+}
--- AppointmentManager/AppMobile/AppointmentManager/Views/MainView.xaml.cs
+++ AppointmentManager/AppMobile/AppointmentManager/Views/MainView.xaml.cs
@@ -1,11 +1,12 @@
-﻿using System;
+﻿using AppointmentManager.Resources.Controls;
+using System;
 using System.Collections.Generic;
 
 using Xamarin.Forms;
 
 namespace AppointmentManager.Views
 {
-    public partial class MainView : ContentPage
+    public partial class MainView : PageBase
     {
         public MainView()
         {
