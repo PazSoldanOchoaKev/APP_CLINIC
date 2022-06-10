1bc974bfda9589a4c434445678542dc4663f532e
t03861 <stalinpazsoldanochoa@bcp.com.pe>
2022-06-10 15:27:43 -0500

    changes

--- AppointmentManager/Microservices/App.Application/Services/AppointmentManager.cs
+++ AppointmentManager/Microservices/App.Application/Services/AppointmentManager.cs
@@ -1,6 +1,6 @@
 ﻿using App.Domain.Entities;
 using App.Domain.Models;
-using Microsoft.Exchange.WebServices.Data;
+using Microsoft.EntityFrameworkCore;
 using Netcos;
 using Netcos.EntityFrameworkCore;
 using System;
@@ -24,25 +24,8 @@ namespace App.Application.Services
             _appointments = appointment;
             _pets = pets;
         }
-        public Task CreatApointmentAsync(AppointmentModel model)
-        {
-            var appointment = _appointments.Include(a => a.Pet).Where(a => a.Pet.UserId == UserId).List();
-            var data = _appointments
-                .Join(_pets, a => a.PetId, p => p.Id, (a, p) => new { a, p })
-                .Where(i => i.p.UserId == userId)
-                .Select(i => new Appointment
-                {
-                    DateAppointment = i.a.DateAppointment,
-                    Hour = i.a.Hour,
-                    Pets = i.p,
-                    Status = i.a.Status,
-                    Specificaction = i.a.Specificaction,
-                })
-                .ToList();
-            return Task.CompletedTask;
-        }
 
-        /*public async Task<Result> CreatAppointmentAsync(AppointmentModel model)
+        public async Task<Result> CreatAppointmentAsync(AppointmentModel model)
         {
             var result = await _appointments.AddAsync(model);
             if (!result)
@@ -54,18 +37,7 @@ namespace App.Application.Services
 
         public Result<IEnumerable<Appointment>> GetAppointmentByUser(string userId)
         {
-            return _appointments
-                .Join(_pets.AsEnumerable(), a => a.PetId, p => p.Id, (a, p) => new { a, p })
-                .Where(i => i.p.UserId == userId)
-                .Select(i => new Appointment
-                {
-                    DateAppointment = i.a.DateAppointment,
-                    Hour = i.a.Hour,
-                    Pets = i.p,
-                    Status = i.a.Status,
-                    Specificaction = i.a.Specificaction,
-                })
-                .ToList();
-        }*/
+            return _appointments.Include(a => a.Pets).Where(a => a.Pets.UserId == userId).ToList();
+        }
     }
 }
