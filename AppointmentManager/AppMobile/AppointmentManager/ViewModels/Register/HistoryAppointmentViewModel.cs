516dc07077d41d1d0a9ccd1d701a8b35991fb197
Kevin Paz Soldan Ochoa <kevin_pazsoldan@hotmail.com>
2022-06-14 04:00:14 -0500

    HistoryAppointment

--- AppointmentManager/AppMobile/AppointmentManager/ViewModels/Register/HistoryAppointmentViewModel.cs
+++ AppointmentManager/AppMobile/AppointmentManager/ViewModels/Register/HistoryAppointmentViewModel.cs
@@ -1,4 +1,6 @@
 ﻿using AppointmentManager.Models;
+using Netcos.Extensions.Http;
+using Netcos.IO.IsolatedStorage;
 using Netcos.Mvvm;
 using Netcos.Xamarin.Forms;
 using System;
@@ -10,20 +12,28 @@ namespace AppointmentManager.ViewModels.Register
 {
     public class HistoryAppointmentViewModel : ViewModelBase, INavigated
     {
-        
+
         private readonly IAppNavigation _navigation;
         private bool isRefresh;
+        private readonly ISecureStorage _storage;
+        private readonly IApiClientFactory _apiClientFactory;
+        private ObservableCollection<NewApointmentModel> appointment;
 
         public HistoryAppointmentViewModel(
+            ISecureStorage storage,
+            IApiClientFactory apiClientFactory,
             IAppNavigation navigation
             )
         {
+            _storage = storage;
+            _apiClientFactory = apiClientFactory;
             _navigation = navigation;
         }
 
 
         #region Properties
-        //public ObservableCollection<AnimalInformationModel> Pets { get => pets; set => SetProperty(ref pets, value); }
+
+        public ObservableCollection<NewApointmentModel> Appointment { get => appointment; set => SetProperty (ref appointment, value); }
         public bool IsRefresh { get => isRefresh; set => SetProperty(ref isRefresh, value); }
         #endregion
 
@@ -32,8 +42,21 @@ namespace AppointmentManager.ViewModels.Register
         #endregion
 
         #region Methodos
-        public void OnNavigated()
+        public async void OnNavigated()
         {
+            var user = await _storage.GetValueAsync<UserModel>(MainViewModel.user);
+            using (var client = _apiClientFactory.CreateClient())
+            {
+                IsRefresh = true;
+                var result = await client
+                    .AppendPath($"appointment/{user.Id}")
+                    .GetAsAsync<List<NewApointmentModel>>();
+                if (result)
+                {
+                    Appointment = new ObservableCollection<NewApointmentModel>(result.Value);
+                }
+                IsRefresh = false;
+            }
         }
         #endregion
     }
