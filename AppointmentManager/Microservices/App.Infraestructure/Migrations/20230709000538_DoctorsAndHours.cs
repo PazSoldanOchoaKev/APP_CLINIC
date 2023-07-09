ea5bb889f766a071cfea015011b82b082b85ce7e
PazSoldanOchoaKev <kevin_pazsoldan@hotmail.com>
2023-07-08 19:06:55 -0500

    Migracion

---
+++ AppointmentManager/Microservices/App.Infraestructure/Migrations/20230709000538_DoctorsAndHours.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace App.Infraestructure.Migrations
+{
+    public partial class DoctorsAndHours : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Users_Access_AccessId",
+                table: "Users");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "AccessId",
+                table: "Users",
+                type: "nvarchar(450)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Users_Access_AccessId",
+                table: "Users",
+                column: "AccessId",
+                principalTable: "Access",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Users_Access_AccessId",
+                table: "Users");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "AccessId",
+                table: "Users",
+                type: "nvarchar(450)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Users_Access_AccessId",
+                table: "Users",
+                column: "AccessId",
+                principalTable: "Access",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
