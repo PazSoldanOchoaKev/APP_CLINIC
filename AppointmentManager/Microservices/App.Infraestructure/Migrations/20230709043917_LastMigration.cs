a990b7ba8ddd633605defcb5667e4e3159924dd6
PazSoldanOchoaKev <kevin_pazsoldan@hotmail.com>
2023-07-08 23:40:48 -0500

    LastMigatrion

---
+++ AppointmentManager/Microservices/App.Infraestructure/Migrations/20230709043917_LastMigration.cs
@@ -0,0 +1,99 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace App.Infraestructure.Migrations
+{
+    public partial class LastMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ListSizes",
+                table: "Appointments");
+
+            migrationBuilder.DropColumn(
+                name: "TypeProcedures",
+                table: "Appointments");
+
+            migrationBuilder.AddColumn<string>(
+                name: "DoctorId",
+                table: "Appointments",
+                type: "nvarchar(450)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "TypeProcedureId",
+                table: "Appointments",
+                type: "nvarchar(450)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Appointments_DoctorId",
+                table: "Appointments",
+                column: "DoctorId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Appointments_TypeProcedureId",
+                table: "Appointments",
+                column: "TypeProcedureId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Appointments_Doctors_DoctorId",
+                table: "Appointments",
+                column: "DoctorId",
+                principalTable: "Doctors",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Appointments_ProcedureTypes_TypeProcedureId",
+                table: "Appointments",
+                column: "TypeProcedureId",
+                principalTable: "ProcedureTypes",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Appointments_Doctors_DoctorId",
+                table: "Appointments");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Appointments_ProcedureTypes_TypeProcedureId",
+                table: "Appointments");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Appointments_DoctorId",
+                table: "Appointments");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Appointments_TypeProcedureId",
+                table: "Appointments");
+
+            migrationBuilder.DropColumn(
+                name: "DoctorId",
+                table: "Appointments");
+
+            migrationBuilder.DropColumn(
+                name: "TypeProcedureId",
+                table: "Appointments");
+
+            migrationBuilder.AddColumn<int>(
+                name: "ListSizes",
+                table: "Appointments",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "TypeProcedures",
+                table: "Appointments",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
