97f48a25c8ab6cce4102c90c81504ca53f503b43
Stalin Paz Soldan Ochoa <spazsoldan@MACLIM-ANACALSOF01.local>
2023-07-09 00:17:45 -0500

    changes

--- AppointmentManager/Microservices/App.Domain/Entities/Appointment.cs
+++ AppointmentManager/Microservices/App.Domain/Entities/Appointment.cs
@@ -15,19 +15,17 @@ namespace App.Domain.Entities
         [Required]
         public string TypeProcedureId { get; set; }
         [Required]
+        public string DoctorId { get; set; }
+        [Required]
         public DateTime DateAppointment { get; set; }
         [Required]
         public TimeSpan Hour { get; set; }
-        [Required]
-        public string DoctorId { get; set; }
         [Required]
         public AppoinmentStatus Status { get; set; }
         public string Specificaction { get; set; }
 
         [ForeignKey(nameof(PetId))]
         public Pets Pets { get; set; }
-        [ForeignKey(nameof(TypeProcedureId))]
-        public ProcedureTypes ProcedureType { get; set; }
         [ForeignKey(nameof(DoctorId))]
         public Doctors Doctor { get; set; }
     }
